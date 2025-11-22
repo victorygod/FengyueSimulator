d84c753aee4ea7e8ccd1168c8cc356635d706db2
Hantian Li <htli0719@outlook.com>
2025-11-22 23:39:32 +0800

    add platform_ios.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 统一入口文件
-支持PC网页服务器和Android应用
+支持PC网页服务器、Android应用和iOS应用
 """
 import argparse
 
@@ -15,12 +15,29 @@ def is_android():
     except ImportError:
         return False
 
+def is_ios():
+    """检测是否在iOS环境中运行"""
+    try:
+        # 检查iOS特定的模块
+        import pyto_ui
+        return True
+    except ImportError:
+        try:
+            import console
+            return True
+        except ImportError:
+            # 检查系统平台
+            import sys
+            if 'ios' in sys.platform:
+                return True
+            return False
+
 
 def main():
     """主函数"""
     parser = argparse.ArgumentParser(description='增强版聊天应用')
-    parser.add_argument('--mode', choices=['web', 'android', 'auto'], default='auto',
-                       help='运行模式: web=网页服务器, android=Android应用, auto=自动检测')
+    parser.add_argument('--mode', choices=['web', 'android', 'ios', 'auto'], default='auto',
+                       help='运行模式: web=网页服务器, android=Android应用, ios=iOS应用, auto=自动检测')
     
     args = parser.parse_args()
     
@@ -28,6 +45,8 @@ def main():
     if args.mode == 'auto':
         if is_android():
             mode = 'android'
+        elif is_ios():
+            mode = 'ios'
         else:
             mode = 'web'
     else:
@@ -49,6 +68,10 @@ def main():
     elif mode == 'android':
         from platform_android import AndroidPlatform
         platform = AndroidPlatform(chat_bot)
+        platform.start()
+    elif mode == 'ios':
+        from platform_ios import IOSPlatform
+        platform = IOSPlatform(chat_bot)
         platform.start()
     else:
         print("错误: 未知的运行模式")
